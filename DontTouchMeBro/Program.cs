0f891e60cd5e6821debcec6d3e98e9b7b0c2bd97
Anthony Headley <1986602+hossimo@users.noreply.github.com>
2023-01-30 10:37:16 -0500

    Initial Commit
    This commit is really just for my Dell XPS 15 with a hard coded Device Instance Path.

---
+++ DontTouchMeBro/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DontTouchMeBro
+{
+    internal static class Program
+    {
+        static NotifyIcon trayIcon;
+
+        static readonly Icon iconYes = Properties.Resources.YesIcon;
+        static readonly Icon iconNo = Properties.Resources.NoIcon;
+
+
+        [STAThread]
+        static void Main()
+        {
+            Mutex mutex = new Mutex(false, "DontTouchMeBro!");
+            if (mutex.WaitOne(0, false))
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                ContextMenu trayMenu = new ContextMenu();
+                trayMenu.MenuItems.Add("Exit", OnExit);
+
+                trayIcon = new NotifyIcon
+                {
+                    Text = "Dont Touch Me Bro",
+                    ContextMenu = trayMenu,
+                    Visible = true
+                };
+
+                trayIcon.Click += OnClick;
+                OnStart();
+                Application.Run();
+            }
+        }
+        static void OnStart()
+        {
+            string result;
+
+            using (Process p = new Process())
+            {
+                p.StartInfo.FileName = (@"C:\Windows\System32\pnputil.exe");
+                p.StartInfo.Verb = "runas";
+                p.StartInfo.UseShellExecute = false;
+                p.StartInfo.CreateNoWindow = true;
+                p.StartInfo.Arguments = "/enum-devices /instanceid \"HID\\ELAN2D25&COL01\\5&2B77D6B&0&0000\"";
+                p.StartInfo.RedirectStandardOutput = true;
+                p.Start();
+                p.WaitForExit();
+                result = p.StandardOutput.ReadToEnd();
+            }
+            if (result.Contains("Started"))
+                trayIcon.Icon = iconYes;
+            else
+                trayIcon.Icon = iconNo;
+        }
+
+        static void OnClick(object sender, EventArgs e)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo(@"C:\Windows\System32\pnputil.exe")
+            {
+                Verb = "runas",
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+
+            if (trayIcon.Icon == iconYes)
+            {
+                startInfo.Arguments = "/disable-device \"HID\\ELAN2D25&COL01\\5&2B77D6B&0&0000\"";
+                trayIcon.Icon = iconNo;
+            }
+            else
+            {
+                startInfo.Arguments = "/enable-device \"HID\\ELAN2D25&COL01\\5&2B77D6B&0&0000\"";
+                trayIcon.Icon = iconYes;
+            }
+            Process.Start(startInfo);
+        }
+
+        static void OnExit(object sender, EventArgs e)
+        {
+            trayIcon.Dispose();
+            Application.Exit();
+        }
+    }
+}
---
+++ DontTouchMeBro/Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DontTouchMeBro")]
+[assembly: AssemblyDescription("Enables and Disables a device by Device Instance Path")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Down Right Technical Inc.")]
+[assembly: AssemblyProduct("DontTouchMeBro")]
+[assembly: AssemblyCopyright("Copyright © Down Right Technical Inc. 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("11ca8063-b9ed-4417-b458-7401a38a89ff")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
